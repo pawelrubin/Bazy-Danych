8bdaee1a8a465d89f5678b7e42cc52d5b6629a2e
pawelrubin <pawelrubin19@gmail.com>
2018-11-12 00:26:59 +0100

    Zadanie 15. poprawione.
    Rak, w postaci niepotrzebnego widoku i JOINa, wyleczony.

--- lista1/15.sql
+++ lista1/15.sql
@@ -1,11 +1,6 @@
 CREATE VIEW avg_amount_for_customers AS
-SELECT c.customer_id, AVG(p.amount) AS avg_amount
-FROM customer c JOIN payment p on c.customer_id = p.customer_id
-GROUP BY c.customer_id;
+SELECT customer_id, AVG(amount) AS avg_amount
+FROM payment
+GROUP BY customer_id;
 
-CREATE VIEW avg_amount_on_2005_07_07 AS
-SELECT AVG(p.amount) AS avg_0707
-FROM customer c JOIN payment p on c.customer_id = p.customer_id
-WHERE payment_date LIKE '2005-07-07%';
-
-select customer_id from avg_amount_for_customers JOIN avg_amount_on_2005_07_07 WHERE avg_amount > avg_0707
+select customer_id from avg_amount_for_customers WHERE avg_amount > (SELECT AVG(amount) FROM payment WHERE payment_date LIKE '2005-07-07%');
