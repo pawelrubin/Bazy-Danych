f95fc1521e7a1f8db483a34a5f25728bf629124f
pawelrubin <pawelrubin19@gmail.com>
2018-11-12 00:51:48 +0100

    Update lista1/16.sql

--- lista1/16.sql
+++ lista1/16.sql
@@ -1,2 +1,5 @@
+ALTER TABLE language
+ADD COLUMN films_no INT NOT NULL AFTER name;
+
 UPDATE language SET films_no = (SELECT COUNT(title) from film WHERE film.language_id=language.language_id );
 select * from language
