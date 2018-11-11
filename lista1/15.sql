6f6f36dfd57895559976c998b283365539b72da4
pawelrubin <pawelrubin19@gmail.com>
2018-11-12 00:30:07 +0100

    Update lista1/15.sql
    a kod zapomniałem poprawić.

--- lista1/15.sql
+++ lista1/15.sql
@@ -3,4 +3,6 @@ SELECT customer_id, AVG(amount) AS avg_amount
 FROM payment
 GROUP BY customer_id;
 
-select customer_id from avg_amount_for_customers WHERE avg_amount > (SELECT AVG(amount) FROM payment WHERE payment_date LIKE '2005-07-07%');
+SELECT customer_id
+FROM avg_amount_for_customers
+WHERE avg_amount > (SELECT AVG(amount) FROM payment WHERE payment_date LIKE '2005-07-07%');
