0a382b74e378d1410df826c48516d4608839923b
pawelrubin <pawelrubin19@gmail.com>
2018-11-11 18:14:08 +0100

    Update lista1/13.sql
    Task 13 fixed.

--- lista1/13.sql
+++ lista1/13.sql
@@ -1,5 +1,7 @@
 SELECT DISTINCT last_name
-FROM actor
-  JOIN film_actor fa on actor.actor_id = fa.actor_id
-  JOIN film f on fa.film_id = f.film_id
-WHERE title NOT LIKE 'B%'
+FROM actor JOIN film_actor fa on actor.actor_id = fa.actor_id
+WHERE actor.actor_id NOT IN (
+  SELECT actor_id
+  FROM film_actor JOIN film ON film_actor.film_id = film.film_id
+  where title LIKE 'B%'
+)
