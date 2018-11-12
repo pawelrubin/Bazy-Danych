463b6449d38b0f60577863d5149a085d02e312b6
pawelrubin <pawelrubin19@gmail.com>
2018-11-12 01:40:57 +0100

    Update lista1/12.sql

--- lista1/12.sql
+++ lista1/12.sql
@@ -5,8 +5,8 @@ FROM (
          count(*) AS num
   FROM film_actor fa1
   JOIN film_actor fa2 ON fa1.film_id = fa2.film_id
-  JOIN actor ac1 on fa1.actor_id = ac1.actor_id
-  JOIN actor ac2 on fa2.actor_id = ac2.actor_id
+  JOIN actor ac1 ON fa1.actor_id = ac1.actor_id
+  JOIN actor ac2 ON fa2.actor_id = ac2.actor_id
   WHERE fa1.actor_id < fa2.actor_id
   GROUP BY fa1.actor_id, fa2.actor_id
 ) AS fa
