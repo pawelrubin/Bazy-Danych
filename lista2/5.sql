db07ec821be8b8621106673f6097b342d68a0944
pawelrubin <pawelrubin19@gmail.com>
2018-11-13 22:20:11 +0100

    Zadanie piąte.
    Dodano procedurę dodającą 1000 agentów.

---
+++ lista2/5.sql
@@ -0,0 +1,30 @@
+
+
+DELIMITER //
+CREATE PROCEDURE tworzenie_agentow ()
+BEGIN
+  SET @id = 1;
+  label: LOOP
+    IF @id > 200 THEN
+      LEAVE label;
+    END IF;
+
+    INSERT INTO agenci (licencja, nazwa, wiek, typ)
+      VALUES (
+        CONCAT('lic', @id),
+        CONCAT_WS(' ',
+          ELT(FLOOR(RAND()*13+1), 'Kapitan', 'Inspektor', 'Chorąży', 'Pan', 'Profesor', 'Niezwykły', 'Rychu', 'Steve',
+            'Michał', 'Cezary', 'Kierowca', 'Andrzej', 'Zygmunt'),
+          ELT(FLOOR(RAND()*13+1), 'Sztorm', 'Gadżet', 'Torpeda', 'Kierownik', 'Zwyczajny', 'Spider Man', 'Peja', 'Jobs',
+            'Archanioł', 'Pazura', 'Tira', 'Baltazar', 'Dzwon')),
+        FLOOR(RAND()*43+21),
+        ELT(FLOOR(RAND()*3+1), 'osoba indywidualna', 'agencja', 'inny')
+    );
+
+    SET @id = @id +1;
+    ITERATE label;
+  END LOOP;
+END //
+DELIMITER ;
+
+SELECT * from kontrakty;
