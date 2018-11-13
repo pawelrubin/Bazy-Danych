fb10ad4b11cd3cb4eb68fb01f5c0d0d86ce13d59
pawelrubin <pawelrubin19@gmail.com>
2018-11-13 19:03:11 +0100

    Dodano zadanie czwarte.

---
+++ lista2/4.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Agenci (
+  licencja VARCHAR(40) NOT NULL,
+  nazwa VARCHAR(90),
+  wiek INT,
+  typ ENUM('osoba indywidualna', 'agencja', 'inny'),
+  PRIMARY KEY (licencja),
+  CHECK (wiek >= 21)
+);
+
+CREATE TABLE Kontrakty (
+  ID INT NOT NULL AUTO_INCREMENT,
+  agent VARCHAR(30),
+  aktor INT,
+  poczatek DATE,
+  koniec DATE,
+  gaza INT,
+  PRIMARY KEY (ID),
+  FOREIGN KEY (aktor) REFERENCES aktorzy(id_aktora),
+  FOREIGN KEY (agent) REFERENCES Agenci(licencja),
+  CHECK (DATEDIFF(day, poczatek, koniec) >= 1),
+  CHECK (gaza >= 0)
+);
