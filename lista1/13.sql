9973eaf9b1d697602ff0f7633882363709ee206e
pawelrubin <pawelrubin19@gmail.com>
2018-11-15 21:41:20 +0100

    Poprawka w zadaniu 13.

--- lista1/13.sql
+++ lista1/13.sql
@@ -1,7 +1,8 @@
-SELECT DISTINCT last_name
+SELECT last_name
 FROM actor JOIN film_actor fa ON actor.actor_id = fa.actor_id
 WHERE actor.actor_id NOT IN (
   SELECT actor_id
   FROM film_actor JOIN film ON film_actor.film_id = film.film_id
   where title LIKE 'B%'
 )
+GROUP BY fa.actor_id;
--- lista2/5.sql
+++ lista2/5.sql
@@ -2,29 +2,45 @@
 
 DELIMITER //
 CREATE PROCEDURE tworzenie_agentow ()
-BEGIN
-  SET @id = 1;
-  label: LOOP
-    IF @id > 200 THEN
-      LEAVE label;
-    END IF;
+  BEGIN
+    SET @id = 1;
+    label: LOOP
+      IF @id > 1000 THEN
+        LEAVE label;
+      END IF;
 
-    INSERT INTO agenci (licencja, nazwa, wiek, typ)
-      VALUES (
-        CONCAT('lic', @id),
-        CONCAT_WS(' ',
-          ELT(FLOOR(RAND()*13+1), 'Kapitan', 'Inspektor', 'Chorąży', 'Pan', 'Profesor', 'Niezwykły', 'Rychu', 'Steve',
-            'Michał', 'Cezary', 'Kierowca', 'Andrzej', 'Zygmunt'),
-          ELT(FLOOR(RAND()*13+1), 'Sztorm', 'Gadżet', 'Torpeda', 'Kierownik', 'Zwyczajny', 'Spider Man', 'Peja', 'Jobs',
-            'Archanioł', 'Pazura', 'Tira', 'Baltazar', 'Dzwon')),
-        FLOOR(RAND()*43+21),
-        ELT(FLOOR(RAND()*3+1), 'osoba indywidualna', 'agencja', 'inny')
-    );
+      INSERT INTO agenci (licencja, nazwa, wiek, typ)
+        VALUES (
+          CONCAT('lic', @id),
+          CONCAT_WS(' ',
+            ELT(FLOOR(RAND()*13+1), 'Kapitan', 'Inspektor', 'Chorąży', 'Pan', 'Profesor', 'Niezwykły', 'Rychu', 'Steve',
+              'Michał', 'Cezary', 'Kierowca', 'Andrzej', 'Zygmunt'),
+            ELT(FLOOR(RAND()*13+1), 'Sztorm', 'Gadżet', 'Torpeda', 'Kierownik', 'Zwyczajny', 'Spider Man', 'Peja', 'Jobs',
+              'Archanioł', 'Pazura', 'Tira', 'Baltazar', 'Dzwon')),
+          FLOOR(RAND()*43+21),
+          ELT(FLOOR(RAND()*3+1), 'osoba indywidualna', 'agencja', 'inny')
+      );
 
-    SET @id = @id +1;
-    ITERATE label;
-  END LOOP;
-END //
+      SET @id = @id +1;
+      ITERATE label;
+    END LOOP;
+  END //
 DELIMITER ;
 
-SELECT * from kontrakty;
+DELIMITER //
+INSERT INTO kontra
+CREATE PROCEDURE do_kontraktow ()
+  BEGIN
+    SET @id = 1;
+    label: LOOP
+      IF @id > (SELECT COUNT(*) FROM aktorzy) THEN
+        LEAVE label;
+        INSERT INTO kontrakty (ID, aktor, agent)
+        SELECT (
+          @id,
+
+
+                   )
+      END IF;
+    END LOOP;
+  END;
