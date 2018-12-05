f1afab4c1c225021fc3afdc519252bca12fe7c97
pawelrubin <pawelrubin19@gmail.com>
2018-12-05 20:49:14 +0100

    Dodano zadanie pierwsze.

---
+++ lista3/1.sql
@@ -0,0 +1,6 @@
+CREATE INDEX idx_filmy_tutyl ON filmy(tytul);
+
+CREATE INDEX idx_aktorzy_nazwisko ON aktorzy(nazwisko, imie(1));
+
+# Indeks na kolumnie id_aktora juz istnial (foreign key).
+CREATE INDEX idx_zagrali_id_aktora ON zagrali(id_aktora);
