b4e2e105d60e5e0f5ab85616f8b90218f9a0bb9e
pawelrubin <pawelrubin19@gmail.com>
2018-12-06 11:05:19 +0100

    Aktualizacja zadania czwartego.
    Dodano procedurę dodającą nowe rekordy do tabeli Ludzie. (reprodukcja xD)

--- lista3/4.sql
+++ lista3/4.sql
@@ -22,7 +22,8 @@ CREATE TABLE Pracownicy (
   PESEL CHAR(11) NOT NULL ,
   zawod VARCHAR(50),
   pensja FLOAT,
-  PRIMARY KEY (PESEL)
+  PRIMARY KEY (PESEL),
+  FOREIGN KEY (PESEL) REFERENCES Ludzie(PESEL)
 );
 
 DELIMITER //
@@ -115,3 +116,62 @@ VALUES ('7813101234', 'Jan', 'Tacior', '1876-10-01', -7, 0, 45, 'czarny');
 
 SELECT * FROM Ludzie;
 DELETE FROM Ludzie;
+
+DELIMITER //
+CREATE PROCEDURE reprodukcja(IN ile INT)
+  BEGIN
+    DECLARE new_imie VARCHAR(30);
+    DECLARE new_nazwisko VARCHAR(30);
+    DECLARE new_data_urodzenia DATE;
+    DECLARE new_wzrost FLOAT;
+    DECLARE new_waga FLOAT;
+    DECLARE new_rozmiar_buta INT(11);
+    DECLARE new_ulubiony_kolor VARCHAR(10);
+    DECLARE new_PESEL CHAR(11);
+    DECLARE PESEL_data CHAR(6);
+    DECLARE PESEL_seria CHAR(4);
+    DECLARE PESEL_suma CHAR(1);
+
+    DECLARE i INT DEFAULT 0;
+    WHILE i < ile DO
+      SET new_imie = ELT(FLOOR(1 + RAND() * 13), 'Kapitan', 'Inspektor', 'Chorąży', 'Pan', 'Profesor', 'Niezwykły', 'Rychu', 'Steve',
+              'Michał', 'Cezary', 'Kierowca', 'Andrzej', 'Zygmunt');
+      SET new_nazwisko = ELT(FLOOR(1 + RAND() * 13), 'Sztorm', 'Gadżet', 'Torpeda', 'Kierownik', 'Zwyczajny', 'Spider Man', 'Peja', 'Jobs',
+              'Archanioł', 'Pazura', 'Tira', 'Baltazar', 'Dzwon');
+      SET new_data_urodzenia = CURDATE() - INTERVAL FLOOR(18 + RAND() * 52) YEAR - INTERVAL FLOOR(RAND() * 365) DAY;
+      SET new_wzrost = FLOOR(140 + RAND() * 80);
+      SET new_waga = FLOOR(40 + RAND() * 100);
+      SET new_rozmiar_buta = FLOOR(36 + RAND() * 10);
+      SET new_ulubiony_kolor = ELT(FLOOR(1 + RAND() * 5), 'czarny', 'czerwony', 'zielony', 'niebieski','biały');
+      SET PESEL_data = CONCAT(
+        SUBSTRING(new_data_urodzenia, 3, 2),
+        SUBSTRING(new_data_urodzenia, 6, 2),
+        SUBSTRING(new_data_urodzenia, 9, 2));
+      SET PESEL_seria = CONCAT(
+          FLOOR(RAND() * 10),
+          FLOOR(RAND() * 10),
+          FLOOR(RAND() * 10),
+          FLOOR(RAND() * 10)
+        );
+      SET PESEL_suma = CAST(MOD(
+        9*CAST(SUBSTRING(PESEL_data, 1, 1) AS UNSIGNED) +
+        7*CAST(SUBSTRING(PESEL_data, 2, 1) AS UNSIGNED) +
+        3*CAST(SUBSTRING(PESEL_data, 3, 1) AS UNSIGNED) +
+        1*CAST(SUBSTRING(PESEL_data, 4, 1) AS UNSIGNED) +
+        9*CAST(SUBSTRING(PESEL_data, 5, 1) AS UNSIGNED) +
+        7*CAST(SUBSTRING(PESEL_data, 6, 1) AS UNSIGNED) +
+        3*CAST(SUBSTRING(PESEL_seria, 1, 1) AS UNSIGNED) +
+        1*CAST(SUBSTRING(PESEL_seria, 2, 1) AS UNSIGNED) +
+        9*CAST(SUBSTRING(PESEL_seria, 3, 1) AS UNSIGNED) +
+        7*CAST(SUBSTRING(PESEL_seria, 4, 1) AS UNSIGNED),
+        10) AS CHAR);
+      SET new_PESEL = CONCAT(PESEL_data, PESEL_seria, PESEL_suma);
+
+      INSERT INTO Ludzie(PESEL, imie, nazwisko, data_urodzenia, wzrost, waga, rozmiar_buta, ulubiony_kolor)
+      VALUES (new_PESEL, new_imie, new_nazwisko, new_data_urodzenia, new_wzrost, new_waga, new_rozmiar_buta, new_ulubiony_kolor);
+      SET i = i + 1;
+    END WHILE;
+  END //
+
+CALL reprodukcja(200);
+
