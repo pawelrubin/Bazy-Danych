b24b047770df327759fa7e60486adf9a924cd39c
pawelrubin <pawelrubin19@gmail.com>
2019-01-20 01:43:25 +0100

    Zadanko numer 14

--- Mongo/14.py
+++ Mongo/14.py
@@ -5,4 +5,50 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 MFilmoteka = myclient["MFilmoteka"]
 agenci = MFilmoteka["agenci"]
 
+bez_liter = agenci.find(
+  {
+    'name': {
+      '$regex': '^[^vxq].*|Kevin',
+      '$options': 'i'
+    },
+    'surname': {
+      '$regex': '^[^vxq].*',
+      '$options': 'i'
+    }
+  },
+  {
+    '_id': 0,
+    'name': 1,
+    'surname': 1,
+    'corpo': 1
+  }
+)
 
+bez_liter_korpo = agenci.find(
+  {
+    'name': {
+      '$regex': '^[^vxq].*|Kevin',
+      '$options': 'i'
+    },
+    'surname': {
+      '$regex': '^[^vxq].*',
+      '$options': 'i'
+    },
+    'corpo': {
+      '$ne': None
+    }
+  },
+  {
+    '_id': 0,
+    'name': 1,
+    'surname': 1,
+    'corpo': 1
+  }
+)
+for i in bez_liter:
+  pprint(i)
+
+print("")
+
+for i in bez_liter_korpo:
+  pprint(i)
--- Mongo/4.py
+++ Mongo/4.py
@@ -1,4 +1,5 @@
 import pymongo
+import random
 from random import randint
 from tools import randelem
 import time
@@ -10,27 +11,42 @@ MFilmoteka = myclient["MFilmoteka"]
 agenci = MFilmoteka["agenci"]
 aktorzy = MFilmoteka["aktorzy"]
 
+agenci.delete_many({})
+
 names = ["Hugh", "Al", "Jan", "Paweł", "Szymon", "Karol", "Michał", "Łukasz",
-  "Dawid", "Mateusz"]
+  "Dawid", "Mateusz", "Kevin", "Xawery", "Qurczak", "Vampir"]
 
 surnames = ["Tarantino", "Bratos", "Cichoń", "Krupski", "Zawada", "Syga", "Żeberski",
-  "Sulkowski", "Macyna", "Kapelko"]
+  "Sulkowski", "Macyna", "Kapelko", "Xanax", "Vąż", "Qrde"]
 
 countries = ["Polska", { "country": "Niemcy", "state": "Bawaria"}, "Australia", { "country": "USA", "state": "Kalifornia" }, 
   { "country": "Rosja", "state": "Moskwa" }]
 
+corpo = ["Avengers", "Hajsownicy", "W4", "Wixapol"]
+
+aktorzy_ids = aktorzy.find().distinct("_id")
+
 def addClients():
   result = []
-  
+  ids = random.sample(aktorzy_ids, randint(1, 5))
+  for i in ids:
+    result.append(
+      {
+        "actor_id": i,
+        "status": randint(0, 1)
+      }
+    )
   return result
 
 for i in range(0, 15):
-  clients = addClients()
   record = { 
     "name": randelem(names), 
     "surname": randelem(surnames),
     "country": randelem(countries),
-    "clients": clients
+    "corpo": randelem(corpo) if randint(0, 1) else None,
+    "clients": addClients()
   }
+  agenci.insert_one(record)
   
-  
+for i in agenci.find({}, {'_id': 0}):
+  pprint(i)
